25959803ab7fef74b673c24cc9f55771982dfb52
Vadim <darksteellord@gmail.com>
2021-08-03 16:26:25 +0300

    interfaces

--- src/contracts/Interfaces.sol
+++ src/contracts/Interfaces.sol
@@ -3,6 +3,25 @@ pragma ton-solidity >= 0.35.0;
 pragma AbiHeader expire;
 pragma AbiHeader time;
 
-interface IElector {}
+interface IElector {
+    // ELECTION PHASE
+    function signUp() external;
+    function elect() external; // ends election
 
-interface IValidator {}
+    // VALIDATION PHASE
+    function revealQuotationMoment(uint moment, uint256 salt) external;
+    function setValidatorsQuotation(uint128 oneUSDCost) external;
+}
+
+interface IValidator {
+    // ELECTION PHASE
+    function onStakeTransfered(???) external;
+    function signUp(IElector elector) external;
+
+    // VALIDATION PHASE
+    function setQuotation() external;
+    function slash() external;
+
+    // AFTER VALIDATION PHASE
+    function cleanUp() external;
+}
