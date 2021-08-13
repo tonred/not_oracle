a620c1fa9cf60f483e46c2607f5da4b9fa931df1
Vadim <darksteellord@gmail.com>
2021-08-13 12:30:18 +0300

    add hash calculator for using with ts4

---
+++ src/contracts/__Calculator.sol
@@ -0,0 +1,17 @@
+/* solhint-disable */
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+
+contract Contract {
+    constructor() public {
+        tvm.accept();
+    }
+
+    function calc(uint128 value, uint256 salt) external pure returns (uint256 hash) {
+        tvm.accept();
+        TvmBuilder builder;
+        builder.store(value, salt);
+        hash = tvm.hash(builder.toCell());
+    }
+}
